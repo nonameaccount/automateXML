7349ce1894d18759bf9245c8cf3236161545affa
Riley Freeman <riley@tandy1000SX.localdomain>
2022-07-07 14:22:45 -0400

    added directories and config/sql

---
+++ sql/demo_ddl.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS TAB1 (
+        COL1 int,
+        COL2 varchar(50),
+        COL3 varchar(50)
+    );
+
+CREATE TABLE IF NOT EXISTS TAB2 (
+    COL4 varchar(50),
+    COL5 varchar(50)
+);
+
+CREATE TABLE IF NOT EXISTS TAB3 (
+    COL6 varchar(50),
+    COL7 varchar(50),
+    COL8 varchar(50)
+);
---
+++ sql/demo_dml.sql
@@ -0,0 +1,6 @@
+INSERT INTO TAB1 VALUES (1,'Elder Watson Diggs', '1947');
+INSERT INTO TAB1 VALUES (2,'Byron Kenneth Armstrong', '1944');
+INSERT INTO TAB2 VALUES ('ssfa', '4717');
+INSERT INTO TAB2 VALUES ('dunklewo', '1775');
+INSERT INTO TAB3 VALUES ('gqr', 'jkltho', 'rhfuiio');
+INSERT INTO TAB3 VALUES ('fadgwvsd', 'qtqgq', 'joipn');
